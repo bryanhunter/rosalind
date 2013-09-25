523117a2a29427d0c078badccaafb849189ede98
calebphillips <caleb.phillips@gmail.com>
2013-09-25 09:50:07 -0500

    clean up on lexv

--- _calebp+clojure/src/rosalind/lexv.clj
+++ _calebp+clojure/src/rosalind/lexv.clj
@@ -1,23 +1,26 @@
 (ns rosalind.lexv
   (:require [clojure.math.combinatorics :as cmb]))
 
-
 (defn make-comparator [alphabet]
-  (let [order-values (zipmap alphabet (range))]
+  (let [order-values (zipmap alphabet (range))
+        compare-chars #(.compareTo (order-values %1)
+                                   (order-values %2))]
     (fn [a b]
-      (or (first
-           (remove zero?
-                   (map (fn [an bn]
-                          (.compareTo (order-values an)
-                                      (order-values bn))) a b)))
+      (or (->> (map compare-chars a b)
+               (remove zero?)
+               first)
           0))))
 
-(defn selections [alphabet n]
-  (mapcat (partial cmb/selections alphabet)
-          (range 1 (inc n))))
+(defn create-strings [alphabet n]
+  (->> (range 1 (inc n))
+       (mapcat (partial cmb/selections alphabet))
+       (sort (make-comparator alphabet))
+       (map #(apply str %))))
+
+(assert
+ (= ["D" "DD" "DDD" "DDN" "DDA" "DN" "DND" "DNN" "DNA" "DA" "DAD" "DAN" "DAA" "N" "ND" "NDD" "NDN" "NDA" "NN" "NND" "NNN" "NNA" "NA" "NAD" "NAN" "NAA" "A" "AD" "ADD" "ADN" "ADA" "AN" "AND" "ANN" "ANA" "AA" "AAD" "AAN" "AAA"]
+    (create-strings "DNA" 3)))
 
 (defn run [alphabet n]
-  (doseq [s (map #(apply str %)
-                 (sort (make-comparator alphabet)
-                       (selections alphabet n)))]
+  (doseq [s (create-strings alphabet n)]
     (println s)))
